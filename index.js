92d09fe6da6b6df08a964f094f4efcea36d1172f
gauti123456 <geekygautam1997@gmail.com>
2022-06-27 22:44:02 +0530

    first commitfs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+const port = process.env.PORT || 5000
+
 app.use(function (req, res, next) {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
     res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
@@ -12,6 +14,6 @@ app.get('/',(req,res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.listen(3000,() => {
-    console.log("App is listening on port 5000")
+app.listen(PORT,() => {
+    console.log("App is running perfectly")
 })
