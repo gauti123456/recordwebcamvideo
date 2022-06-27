4ff4e1a27b4918d228300a860ee0e9dfbeefbab9
gauti123456 <geekygautam1997@gmail.com>
2022-06-27 22:45:29 +0530

    first commith

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.use(function (req, res, next) {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
